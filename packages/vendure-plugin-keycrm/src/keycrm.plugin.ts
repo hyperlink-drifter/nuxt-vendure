e5761e51fb5efe84f2780f5a98f14fd865ea1548
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-03-30 20:35:49 +0200

    feat(keycrm): add custom field for keycrm id to vendure product

--- packages/vendure-plugin-keycrm/src/keycrm.plugin.ts
+++ packages/vendure-plugin-keycrm/src/keycrm.plugin.ts
@@ -1,25 +1,37 @@
-import { PluginCommonModule, Type, VendurePlugin } from '@vendure/core';
-
+import {
+  PluginCommonModule,
+  Type,
+  VendurePlugin,
+  mergeConfig,
+} from '@vendure/core';
 import { KEYCRM_PLUGIN_OPTIONS } from './constants';
 import { PluginInitOptions } from './types';
+import { KeycrmService } from './services/keycrm.service';
 
 @VendurePlugin({
-    imports: [PluginCommonModule],
-    providers: [{ provide: KEYCRM_PLUGIN_OPTIONS, useFactory: () => KeycrmPlugin.options }],
-    configuration: config => {
-        // Plugin-specific configuration
-        // such as custom fields, custom permissions,
-        // strategies etc. can be configured here by
-        // modifying the `config` object.
-        return config;
-    },
-    compatibility: '^3.0.0',
+  imports: [PluginCommonModule],
+  providers: [
+    { provide: KEYCRM_PLUGIN_OPTIONS, useFactory: () => KeycrmPlugin.options },
+    KeycrmService,
+  ],
+  configuration: (config) => {
+    // Plugin-specific configuration
+    // such as custom fields, custom permissions,
+    // strategies etc. can be configured here by
+    // modifying the `config` object.
+    return mergeConfig(config, {
+      customFields: {
+        Product: [{ name: 'KeycrmId', type: 'string', nullable: true }],
+      },
+    });
+  },
+  compatibility: '^3.0.0',
 })
 export class KeycrmPlugin {
-    static options: PluginInitOptions;
+  static options: PluginInitOptions;
 
-    static init(options: PluginInitOptions): Type<KeycrmPlugin> {
-        this.options = options;
-        return KeycrmPlugin;
-    }
+  static init(options: PluginInitOptions): Type<KeycrmPlugin> {
+    this.options = options;
+    return KeycrmPlugin;
+  }
 }
