880c5b6a661065aed1e6b55b0b6ab3ce89cfbcae
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-03-31 23:24:05 +0200

    feat(keycrm): extend vendure's built-in custom fields interface to add type safety and autocomplete for custom field of keycrm id
    https://docs.vendure.io/guides/developer-guide/custom-fields/#typescript-typings

--- packages/vendure-plugin-keycrm/src/types.ts
+++ packages/vendure-plugin-keycrm/src/types.ts
@@ -1,3 +1,15 @@
+// types.ts
+
+// Note: we are using a deep import here, rather than importing from `@vendure/core` due to
+// a possible bug in TypeScript (https://github.com/microsoft/TypeScript/issues/46617) which
+// causes issues when multiple plugins extend the same custom fields interface.
+import { CustomProductFields } from '@vendure/core/dist/entity/custom-entity-fields';
+declare module '@vendure/core/dist/entity/custom-entity-fields' {
+  interface CustomProductFields {
+    KeycrmId: string;
+  }
+}
+
 /**
  * @description
  * The plugin can be configured using the following options:
