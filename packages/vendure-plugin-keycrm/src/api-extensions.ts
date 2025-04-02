a993d7f78e52a4efd6795051acd9390538c7780c
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-03 00:44:44 +0200

    feat(keycrm): better type for shop api extension

--- packages/vendure-plugin-keycrm/src/api-extensions.ts
+++ packages/vendure-plugin-keycrm/src/api-extensions.ts
@@ -2,7 +2,19 @@ import { gql } from 'graphql-tag';
 import { DocumentNode } from 'graphql';
 
 export const shopApiExtensions: DocumentNode = gql`
+  type ProductKeycrmToVendure {
+    id: ID!
+    createdAt: DateTime!
+    updatedAt: DateTime!
+    name: String!
+    slug: String
+    description: String
+    enabled: Boolean!
+    featuredAsset: Asset
+    assets: [Asset]
+  }
+
   extend type Product {
-    keycrm: Product!
+    keycrm: ProductKeycrmToVendure!
   }
 `;
