4fe34ba850f43612bc95cbc4a24bddd750256654
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-03 00:47:41 +0200

    refactor(keycrm): cleanup plugin service

--- packages/vendure-plugin-keycrm/src/keycrm.service.ts
+++ packages/vendure-plugin-keycrm/src/keycrm.service.ts
@@ -1,31 +1,18 @@
-import { Inject, Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { Logger, RequestContext } from '@vendure/core';
-import { KEYCRM_PLUGIN_OPTIONS, loggerCtx } from './constants';
+import { Inject, Injectable } from '@nestjs/common';
+import { RequestContext, Product } from '@vendure/core';
+import { KEYCRM_PLUGIN_OPTIONS } from './constants';
 import { PluginInitOptions } from './types';
 import { KeycrmClient } from './keycrm.client';
 
 @Injectable()
-export class KeycrmService implements OnApplicationBootstrap {
+export class KeycrmService {
   constructor(
     @Inject(KEYCRM_PLUGIN_OPTIONS) private options: PluginInitOptions,
     private keycrmClient: KeycrmClient
   ) {}
 
   async getProduct(ctx: RequestContext, KeycrmId: string) {
-    // Add your method logic here
-    Logger.info(`KeycrmId: ${KeycrmId}`, loggerCtx);
-
     const product = await this.keycrmClient.getProduct(KeycrmId);
-
-    Logger.info(`Keycrm Product: ${JSON.stringify(product)}`, loggerCtx);
-
-    return {
-      id: KeycrmId,
-      name: 'this-is-democracy-manifest-t-shirt',
-    };
-  }
-
-  async onApplicationBootstrap() {
-    Logger.info(`Api Key: ${this.options.apiKey}`, loggerCtx);
+    return product;
   }
 }
