61b53f8e2f5875f9c07345516543ca77ad84552a
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-03 22:07:33 +0200

    feat(keycrm): update keycrm service to return translated product

--- packages/vendure-plugin-keycrm/src/keycrm.service.ts
+++ packages/vendure-plugin-keycrm/src/keycrm.service.ts
@@ -1,18 +1,30 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { RequestContext, Product } from '@vendure/core';
+import {
+  RequestContext,
+  Product,
+  Translated,
+  TranslatorService,
+} from '@vendure/core';
 import { KEYCRM_PLUGIN_OPTIONS } from './constants';
 import { PluginInitOptions } from './types';
 import { KeycrmClient } from './keycrm.client';
+import { toVendureProduct } from './keycrm.helpers';
 
 @Injectable()
 export class KeycrmService {
   constructor(
     @Inject(KEYCRM_PLUGIN_OPTIONS) private options: PluginInitOptions,
-    private keycrmClient: KeycrmClient
+    private keycrmClient: KeycrmClient,
+    private translator: TranslatorService
   ) {}
 
-  async getProduct(ctx: RequestContext, KeycrmId: string) {
-    const product = await this.keycrmClient.getProduct(KeycrmId);
-    return product;
+  async getProduct(
+    ctx: RequestContext,
+    product: Product
+  ): Promise<Translated<Product>> {
+    const keycrmId = product.customFields.KeycrmId;
+    const keycrm = await this.keycrmClient.getProduct(keycrmId);
+    const vendure = toVendureProduct(keycrm, product);
+    return this.translator.translate(vendure, ctx);
   }
 }
