cddc9a2c99bfe3e977c0d858e9396aa1faab172b
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-04 16:48:18 +0200

    feat(keycrm): add has_offers property and update type

--- packages/vendure-plugin-keycrm/src/keycrm.helpers.ts
+++ packages/vendure-plugin-keycrm/src/keycrm.helpers.ts
@@ -2,6 +2,8 @@ import { AssetType } from '@vendure/core';
 import type {
   LocaleString,
   Product,
+  ProductOption,
+  ProductOptionGroup,
   Translation,
   VendureEntity,
 } from '@vendure/core';
@@ -10,79 +12,84 @@ import { ProductKeycrm } from './types';
 export function toVendureProduct(
   keycrmProduct: ProductKeycrm,
   vendureProduct: Product
-): Product & { translations: Array<Translation<VendureEntity>> } {
-  const product: Product & { translations: Array<Translation<VendureEntity>> } =
-    {
-      id: keycrmProduct.id,
-      createdAt: keycrmProduct.created_at,
-      updatedAt: keycrmProduct.updated_at,
-      deletedAt: null,
-      name: keycrmProduct.name as LocaleString,
-      slug: vendureProduct.slug,
-      description: keycrmProduct.description
-        ? (keycrmProduct.description as LocaleString)
-        : vendureProduct.description
-        ? vendureProduct.description
-        : ('' as LocaleString),
-      enabled: !keycrmProduct.is_archived,
-      featuredAssetId: 0,
-      featuredAsset: {
-        source: keycrmProduct.thumbnail_url ? keycrmProduct.thumbnail_url : '',
-        // TODO: fall back values to satisfy type
-        name: '',
-        type: AssetType.IMAGE,
-        mimeType: '',
-        width: 0,
-        height: 0,
-        fileSize: 0,
-        preview: '',
-        tags: [],
-        channels: [],
-        id: '',
-        createdAt: keycrmProduct.created_at,
-        updatedAt: keycrmProduct.created_at,
-        customFields: [],
-      },
-      assets: [
-        ...keycrmProduct.attachments_data.map((url) => ({
-          source: url,
-          // TODO: fall back values to satisfy type
-          productId: keycrmProduct.id,
-          product: vendureProduct,
-          assetId: 0,
-          createdAt: keycrmProduct.created_at,
-          updatedAt: keycrmProduct.updated_at,
-          id: 0,
-          asset: {
-            source: url ? url : '',
-            // TODO: fall back values to satisfy type
-            name: '',
-            type: AssetType.IMAGE,
-            mimeType: '',
-            width: 0,
-            height: 0,
-            fileSize: 0,
-            preview: '',
-            tags: [],
-            channels: [],
-            id: '',
-            createdAt: keycrmProduct.created_at,
-            updatedAt: keycrmProduct.created_at,
-            customFields: [],
-          },
-          position: 0,
-        })),
-      ],
-      translations: [],
-      variants: [],
-      optionGroups: [],
-      facetValues: [],
+): Product &
+  Partial<ProductKeycrm> & { translations: Array<Translation<VendureEntity>> } {
+  const product: Product &
+    Partial<ProductKeycrm> & {
+      translations: Array<Translation<VendureEntity>>;
+    } = {
+    id: keycrmProduct.id,
+    createdAt: keycrmProduct.created_at,
+    updatedAt: keycrmProduct.updated_at,
+    deletedAt: null,
+    name: keycrmProduct.name as LocaleString,
+    slug: vendureProduct.slug,
+    description: keycrmProduct.description
+      ? (keycrmProduct.description as LocaleString)
+      : vendureProduct.description
+      ? vendureProduct.description
+      : ('' as LocaleString),
+    enabled: !keycrmProduct.is_archived,
+    featuredAssetId: 0,
+    featuredAsset: {
+      source: keycrmProduct.thumbnail_url ? keycrmProduct.thumbnail_url : '',
+      // TODO: fall back values to satisfy type
+      name: '',
+      type: AssetType.IMAGE,
+      mimeType: '',
+      width: 0,
+      height: 0,
+      fileSize: 0,
+      preview: '',
+      tags: [],
       channels: [],
-      customFields: {
-        KeycrmId: vendureProduct.customFields.KeycrmId,
-      },
-      keycrm: undefined,
-    };
+      id: '',
+      createdAt: keycrmProduct.created_at,
+      updatedAt: keycrmProduct.created_at,
+      customFields: [],
+    },
+    assets: [
+      ...keycrmProduct.attachments_data.map((url) => ({
+        source: url,
+        // TODO: fall back values to satisfy type
+        productId: keycrmProduct.id,
+        product: vendureProduct,
+        assetId: 0,
+        createdAt: keycrmProduct.created_at,
+        updatedAt: keycrmProduct.updated_at,
+        id: 0,
+        asset: {
+          source: url ? url : '',
+          // TODO: fall back values to satisfy type
+          name: '',
+          type: AssetType.IMAGE,
+          mimeType: '',
+          width: 0,
+          height: 0,
+          fileSize: 0,
+          preview: '',
+          tags: [],
+          channels: [],
+          id: '',
+          createdAt: keycrmProduct.created_at,
+          updatedAt: keycrmProduct.created_at,
+          customFields: [],
+        },
+        position: 0,
+      })),
+    ],
+    translations: [],
+    variants: [],
+    optionGroups: [],
+    facetValues: [],
+    channels: [],
+    customFields: {
+      KeycrmId: vendureProduct.customFields.KeycrmId,
+    },
+    keycrm: undefined,
+    // fields specific to keycrm product
+    has_offers: keycrmProduct.has_offers,
+  };
 
   return product;
 }
