f9f54f383e845913fe5d2bd0f5dba708a1228365
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-05 20:52:52 +0200

    feat(keycrm): override field language-code of product-option-entity resolver -- not supported

--- packages/vendure-plugin-keycrm/src/resolvers/keycrm.product-option-entity.resolver.ts
+++ packages/vendure-plugin-keycrm/src/resolvers/keycrm.product-option-entity.resolver.ts
@@ -1,10 +1,18 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { ProductOptionPicked } from '../types';
+import { InternalServerError } from '@vendure/core';
 
 @Resolver('ProductOption')
 export class ProductOptionEntityResolver {
   @ResolveField()
   name(@Parent() productOption: ProductOptionPicked): Promise<string> {
     return Promise.resolve(productOption.name);
+  }
+
+  @ResolveField()
+  languageCode() {
+    throw new InternalServerError(
+      'error.keycrm-plugin.product-option.language-code.field-not-supported'
+    );
   }
 }
