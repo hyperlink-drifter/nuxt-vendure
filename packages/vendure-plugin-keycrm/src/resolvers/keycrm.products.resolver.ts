b8a9d8b305cdf5cb9129563f5a076414d23f9ee7
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-06 23:10:07 +0200

    feat(keycrm): define resolver for keycrm-product

--- packages/vendure-plugin-keycrm/src/resolvers/keycrm.products.resolver.ts
+++ packages/vendure-plugin-keycrm/src/resolvers/keycrm.products.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Scalars } from '@vendure/core';
+import { KeycrmClient } from '../keycrm.client';
+import { ProductKeycrm } from '../types';
+
+type QueryKeycrmProductArgs = {
+  id: Scalars['ID'];
+};
 
 @Resolver()
-export class ShopKeycrmProductResolver {}
+export class ShopKeycrmProductResolver {
+  constructor(private keycrmClient: KeycrmClient) {}
+
+  @Query()
+  async keycrmProduct(
+    @Args() args: QueryKeycrmProductArgs
+  ): Promise<ProductKeycrm> {
+    const keycrmProduct = await this.keycrmClient.getProduct(args.id);
+    return keycrmProduct;
+  }
+}
