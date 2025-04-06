2c2fa7aaa70f33966b978b1d1299c21023f53038
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-06 22:55:08 +0200

    feat(keycrm): define graphql type for keycrm-product-offer

--- packages/vendure-plugin-keycrm/src/api-extensions.ts
+++ packages/vendure-plugin-keycrm/src/api-extensions.ts
@@ -2,6 +2,31 @@ import gql from 'graphql-tag';
 import { DocumentNode } from 'graphql';
 
 export const shopApiExtensions: DocumentNode = gql`
+  type KeycrmProductOffer implements Node {
+    id: ID!
+    name: String!
+    description: String
+    thumbnail_url: String
+    attachments_data: [String]
+    quantity: Int
+    unit_type: String
+    currency_code: String
+    sku: String
+    min_price: Float
+    max_price: Float
+    weight: Float
+    length: Float
+    width: Float
+    height: Float
+    has_offers: Boolean
+    is_archived: Boolean
+    category_id: ID
+    created_at: DateTime!
+    updated_at: DateTime!
+    offers: [KeycrmOffer]
+    properties_agg: [KeycrmOfferPropertyAgg]
+  }
+
   type KeycrmProduct implements Node {
     id: ID!
     name: String!
