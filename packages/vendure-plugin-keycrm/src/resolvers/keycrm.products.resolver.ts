c73ba30bbc06e4c0a80114de8b782fc4dbb52357
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-07 21:52:05 +0200

    feat(keycrm): define graphq resolver for query keycrm-products

--- packages/vendure-plugin-keycrm/src/resolvers/keycrm.products.resolver.ts
+++ packages/vendure-plugin-keycrm/src/resolvers/keycrm.products.resolver.ts
@@ -1,15 +1,44 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Scalars } from '@vendure/core';
 import { KeycrmClient } from '../keycrm.client';
-import { ProductKeycrm, ProductOfferKeycrm } from '../types';
+import { ProductKeycrm, ProductListKeycrm, ProductOfferKeycrm } from '../types';
 
 type QueryKeycrmProductArgs = {
   id: Scalars['ID'];
 };
 
+type QueryKeycrmProductsArgs = {
+  options: {
+    limit?: Scalars['Int'];
+    page?: Scalars['Int'];
+    filter?: {
+      product_id?: Scalars['String'];
+      category_id?: Scalars['String'];
+      is_archived?: Scalars['Boolean'];
+    };
+  };
+};
+
 @Resolver()
 export class ShopKeycrmProductResolver {
   constructor(private keycrmClient: KeycrmClient) {}
+
+  @Query()
+  async keycrmProducts(
+    @Args() args: QueryKeycrmProductsArgs
+  ): Promise<ProductListKeycrm> {
+    const { options } = args;
+    const { limit, page, filter } = options ?? {};
+    const { product_id, category_id, is_archived } = filter ?? {};
+    const keycrmProductList = await this.keycrmClient.getProducts({
+      ...(limit && { limit }),
+      ...(page && { page }),
+      ...(product_id && { 'filter[product_id]': product_id }),
+      ...(category_id && { 'filter[category_id]': category_id }),
+      ...(is_archived && { 'filter[is_archived]': is_archived }),
+    });
+    return keycrmProductList;
+  }
 
   @Query()
   async keycrmProduct(
