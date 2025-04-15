607a1d8b8123cb0af40e11a3fec1c284b5a86e39
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-15 21:01:24 +0200

    feat(keycrm): define interface extention for custom fields

--- packages/vendure-plugin-keycrm/src/types.ts
+++ packages/vendure-plugin-keycrm/src/types.ts
@@ -1,3 +1,32 @@
+// types.ts
+
+// Note: we are using a deep import here, rather than importing from `@vendure/core` due to
+// a possible bug in TypeScript (https://github.com/microsoft/TypeScript/issues/46617) which
+// causes issues when multiple plugins extend the same custom fields interface.
+import {
+  CustomProductFields,
+  CustomProductVariantFields,
+} from '@vendure/core/dist/entity/custom-entity-fields';
+
+declare module '@vendure/core/dist/entity/custom-entity-fields' {
+  interface CustomProductFields {
+    keycrm: {
+      id: number;
+      created_at: Date;
+      updated_at: Date;
+    };
+  }
+
+  interface CustomProductVariantFields {
+    keycrm: {
+      id: number;
+      product_id: number;
+      created_at: Date;
+      updated_at: Date;
+    };
+  }
+}
+
 /**
  * @description
  * The plugin can be configured using the following options:
