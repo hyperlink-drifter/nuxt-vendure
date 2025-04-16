c71b894256ce43759ab3b86ff2df26013d79f47e
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2025-04-16 20:28:27 +0200

    docs(keycrm): add important todo

--- packages/vendure-plugin-keycrm/src/keycrm.sync.controller.ts
+++ packages/vendure-plugin-keycrm/src/keycrm.sync.controller.ts
@@ -6,6 +6,8 @@ import { KeycrmSyncService } from './keycrm.sync.service';
 export class KeycrmSyncController {
   constructor(private keycrmSyncService: KeycrmSyncService) {}
 
+  // TODO: @Allow(Permission.Entity)
+  // https://docs.vendure.io/reference/typescript-api/request/allow-decorator/
   @Post()
   sync(@Ctx() ctx: RequestContext) {
     return this.keycrmSyncService.sync(ctx);
